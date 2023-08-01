1e093ec92cddd45b5e0feb5c09791572cb3bcc38
BigBrazz <118530212+BigBrazz@users.noreply.github.com>
2023-08-01 16:46:13 +0100

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+import matplotlib as mb 
+
+st.set_page_config(page_title= "Dojo Dashboard", page_icon= "bar_chart", layout= "centered", initial_sidebar_state= "collapsed") 
+st.title("Dojo Dashboard") 
